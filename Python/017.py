c86d402c872b6967116684d119a1ef2538c5e4dc
jkwaters <jkwaters@gmail.com>
2015-07-17 16:20:49 -0400

    Problem 17

---
+++ Python/017.py
@@ -0,0 +1,18 @@
+# Project Euler | Problem 17 | Jacob Waters
+# If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words,
+# how many letters would be used? Do not count spaces or hyphens.
+# 21124
+# date : 2015.01.26
+
+def lettersIn1To1000():
+  print "1 to 9"
+  print "3 + 3 + 5 + 4 + 4 + 3 + 5 + 5 + 4 = 36\n"
+  print "10 to 19"
+  print "3 + 6 + 6 + 8 + 8 + 7 + 7 + 9 + 8 + 8 = 70\n"
+  print "20 to 99"
+  print "..."
+  print "100 to 1000"
+  print "..."
+  print "36 + 70 + "
+
+lettersIn1To1000()
